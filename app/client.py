b0b38eea5fd6b7355383e77f380a06c52e232a11
Tophers <topher@trulytilted.com>
2026-05-05 00:05:57 -0500

    Version v2026.5.2

--- app/client.py
+++ app/client.py
@@ -35,7 +35,7 @@ EMBY_USER = os.environ.get("EMBY_USER")
 EMBY_PASS = os.environ.get("EMBY_PASS")
 
 CLIENT_NAME = "MixerBee"
-CLIENT_VERSION = "2026.05.1"
+CLIENT_VERSION = "2026.05.2"
 DEVICE_ID = "MixerBeePy"
 DEVICE_NAME = "MixerBee"
 
